898d5285d46b11cb8c1ea214ce1d6e382b526714
KTachibanaM <whj19931115@gmail.com>
2024-02-15 19:45:10 -0800

    add miniflux-chilled-refresher

---
+++ miniflux-chilled-refresher/miniflux-chilled-refresher.py
@@ -0,0 +1,26 @@
+import time
+import random
+import logging
+import miniflux
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+# Creating a client using username / password authentication
+client = miniflux.Client("http://miniflux", username="admin", password="test123")
+
+refresh_interval_seconds = 60
+
+while True:
+    feeds = client.get_feeds()
+    for feed in feeds:
+        logging.info(f"Refreshing {feed['feed_url']}")
+        client.refresh_feed(feed['id'])
+        feed_sleep_seconds = refresh_interval_seconds + random.randint(1, refresh_interval_seconds)
+        logging.info(f"Feed sleeping for {feed_sleep_seconds} seconds")
+        time.sleep(feed_sleep_seconds)
+    loop_sleep_seconds = refresh_interval_seconds * len(feeds) * 2
+    logging.info(f"Loop sleeping for {loop_sleep_seconds}; there were {len(feeds)} feeds")
+    time.sleep(loop_sleep_seconds)
